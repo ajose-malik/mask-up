61c0b3c4f2a833ee76112211180fa75abad76e6d
ajose-malik <ajose.malik@gmail.com>
2020-12-05 18:27:06 -0500

    RENAMe helper.js to quotes.js & ADD insert comma function

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
-// $.ajax({
-// 	url: 'https://api.covidtracking.com/v1/us/current.json'
-// }).then(
-// 	data => {
-// 		console.log(data);
-// 	},
-// 	() => {
-// 		console.log('DISASTER');
-// 	}
-// );
+/////////////////////////////////////////////////////////////////////////
+// Random Quotes ///////////////////////////////////////////////////////
+const randQuotes = () => {
+	const rand = () => Math.floor(Math.random() * quotes.length);
+	$('#quote').append(quotes[rand()]);
+	setInterval(() => {
+		$('#quote').empty();
+		$('#quote').append(quotes[rand()]);
+	}, 10000);
+};
+// randQuotes();
+
+///////////////////////////////////////////////////////////////////////////////
+// Data Request for United States ////////////////////////////////////////////
+$.ajax({
+	url: 'https://api.covidtracking.com/v1/us/current.json'
+}).then(
+	data => {
+		data = data[0];
+		const totalCase = String(data.positive);
+		const totalDeath = String(data.death);
+
+		// Insert Comma Function
+		const convertStr = str => {
+			strArr = str.split('');
+			if (strArr.length > 3 && strArr.length < 7) {
+				strArr.splice(-3, 0, ',');
+				return strArr.join('');
+			} else if (strArr.length > 6 && strArr.length < 10) {
+				strArr.splice(-3, 0, ',');
+				strArr.splice(-7, 0, ',');
+				return strArr.join('');
+			}
+		};
+
+		$('#total-case').append(convertStr(totalCase));
+		$('#total-death').append(convertStr(totalDeath));
+
+		// Data Request for Each State ///////////////////
+		// $.ajax({
+		// 	url: 'https://api.covidtracking.com/v1/us/current.json'
+		// }).then(
+		// 	data => {
+		// 		console.log(data);
+		// 	},
+		// 	() => {
+		// 		console.log('Error Each States');
+		// 	}
+		// );
+	},
+	() => {
+		console.log('Error United States');
+	}
+);
+
+/////////////////////////////////////////////////////////////////////////////
+// DOM Functions //////////////////////////////////////////////////////////
+
+// Toggle Intro Slide
+$('#mask-up h1 #title').click(() => {
+	$(
+		'#mask-up #quote, #mask-up p, #mask-up #license-icon, #mask-up h1 #enter'
+	).toggle();
+});
--- quotes.js
+++ quotes.js
@@ -4,7 +4,6 @@ const quotes = [
 	'Ultimately, the greatest lesson that COVID-19 can teach humanity is that we are all in this together — Kiran Mazumdar-Shaw.',
 	'Life doesn’t get easier or more forgiving, we get stronger and more resilient — Steve Maraboli.',
 	'No matter how much falls on us, we keep plowing ahead. That’s the only way to keep the roads clear — Greg Kincaid.',
-	'Women are working more, men are understanding their value as caregivers, women are primary breadwinners—I mean, we could go on and on and on. Things are different. So we can’t keep operating like everything is the same, and that’s what many of us have done. And I think it’s up to us to change the conversation — Michelle Obama.',
 	'In the face of adversity, we have a choice. We can be bitter, or we can be better. Those words are my North Star — Caryn Sullivan.',
 	`I've got some bad news and I've got some good news. Nothing lasts forever — Kate McGahan.`,
 	'Losing your head in a crisis is a good way to become a crisis — C.J. Redwine.',
@@ -17,7 +16,6 @@ const quotes = [
 	'We are, or will be, going through the most radical transformation the world has ever seen; people are justly terrified, excited, depressed, heartbroken and hopeful, all at once — Heather Marsh.',
 	'In every crisis, doubt or confusion, take the higher path - the path of compassion, courage, understanding and love — Amit Ray.',
 	'Center of your heart is the center of the Universe. Go to that center and radiate positive vibration for the well-being of the humanity — Amit Ray.',
-	`We have a chance to do something extraordinary. As we head out of this pandemic we can change the world. Create a world of love. A world where we are kind to each other. A world were we are kind no matter what class, race, sexual orientation, what religion or lack of or what job we have. A world we don't judge those at the food bank because that may be us if things were just slightly different. Let love and kindness be our roadmap — Johnny Corn.`,
 	'These so-called bleak times are necessary to go through in order to get to a much, much better place — David Lynch.',
 	'COVID-19, are you going to be naughty or nice to me? — Steven Magee.',
 	'A person in public without a mask during a pandemic is a walking septic tank — Abhijit Naskar.',
@@ -26,8 +24,3 @@ const quotes = [
 	`And our hearts cry, "How long? How far? How much more?" — Nicole Williams.`,
 	'Without doubt, maintaining a distance from some people is the only way to maintaining your equanimity – and, of course, to avoid catching COVID-19! — AVIS Viswanathan.'
 ];
-
-const randQuotes = () => {
-	const rand = Math.floor(Math.random() * quotes.length);
-	console.log(quotes[rand]);
-};
